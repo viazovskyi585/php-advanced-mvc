7fb35c51f8b32b2e3538a8a6ab4535970b10716c
evgeniy <vyazovsky.ea@gmail.com>
2023-05-29 15:34:48 +0300

    config

--- public/index.php
+++ public/index.php
@@ -1,2 +1,22 @@
 <?php
-phpinfo();
+use Config\Config;
+use Dotenv\Dotenv;
+require_once dirname(__DIR__) . '/Config/constants.php';
+require_once BASE_DIR . '/vendor/autoload.php';
+
+try {
+    if (!session_id()) {
+        session_start();
+    }
+
+    $dotenv = Dotenv::createUnsafeImmutable(BASE_DIR);
+    $dotenv->load();
+
+
+	$host = Config::get('db.user');
+
+	echo $host;
+
+} catch (Exception $exception) {
+    echo "Exception: " . $exception->getMessage();
+}
