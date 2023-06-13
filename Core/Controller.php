99224f6b459922351c8029632c7d0c50168373c9
evgeniy <vyazovsky.ea@gmail.com>
2023-06-13 12:45:59 +0300

    deleted comments

--- Core/Controller.php
+++ Core/Controller.php
@@ -1,17 +1,15 @@
 <?php
+
 namespace Core;
 
 class Controller
 {
 	public function before(string $action): bool
 	{
-		// echo "before $action";
-
 		return true;
 	}
- 
+
 	public function after(string $action): void
 	{
-		// echo "after $action";
 	}
 }
